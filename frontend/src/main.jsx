a14d76736ec48d10af5df2c3f97ff02b353804cc
sude-ocall <sudegl.2003@gmail.com>
2026-04-05 21:07:56 +0300

    bug fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
 
-import './index.css'
+// ─── Bootstrap CSS & JS ──────────────────────────────
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
+import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
